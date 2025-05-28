5e42c9ecb3a21ac9c97b27c17a0e686ac740f9c6
Samuel-John-Wilson <Samuel.John.Wilson@outlook.com>
2025-05-28 22:05:59 +1000

    0.0.6: added extract_markdown_images and _links, basic testcases

---
+++ src/test_extract_markdown.py
@@ -0,0 +1,20 @@
+import unittest
+from extract_markdown_func import extract_markdown_images, extract_markdown_links
+
+
+class Test_extract_markdown_images(unittest.TestCase):
+    def test_extract_markdown_images(self):
+            matches = extract_markdown_images("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)")
+            self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    
+    def test_extract_markdown_multiple_images(self):
+            matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) or two ![image](https://i.imgur.com/zjjcJKZ.png)")
+            self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png"), ("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+
+class Test_extract_markdown_links(unittest.TestCase):
+       def test_extract_markdown_links(self):
+            matches = extract_markdown_links("This is text with a link"
+            " [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)")
+            self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
