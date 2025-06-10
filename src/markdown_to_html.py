f76d18a255115cc70ca4d5351d69d4096153d8b3
Samuel-John-Wilson <Samuel.John.Wilson@outlook.com>
2025-06-10 20:34:07 +1000

    0.1.1.2: imports

--- src/markdown_to_html.py
+++ src/markdown_to_html.py
@@ -1,15 +1,28 @@
 
-"""Assignment
-Create a new function called def markdown_to_html_node(markdown): that converts a full markdown document into a single parent HTMLNode. That one parent HTMLNode should (obviously) contain many child HTMLNode objects representing the nested elements.
+from blocktype import blocktype
+from block_func import markdown_to_blocks, block_to_blocktype
+from converter_func import text_node_to_html_node, split_nodes_delimiter, split_nodes_image, split_nodes_link, text_to_textnodes
+from htmlnode import HTMLNode, ParentNode, LeafNode
+from textnode import TextNode, TextType
 
-FYI: I created an additional 8 helper functions to keep my code neat and easy to understand, because there's a lot of logic necessary for markdown_to_html_node. I don't want to give you my exact functions because I want you to do this from scratch. However, I'll give you the basic order of operations:
+
+
+"""Assignment
+Create a new function called def markdown_to_html_node(markdown): that converts a full markdown document into a single parent HTMLNode.
+That one parent HTMLNode should (obviously) contain many child HTMLNode objects representing the nested elements.
+
+FYI: I created an additional 8 helper functions to keep my code neat and easy to understand, because there's a lot of logic necessary for markdown_to_html_node.
+ I don't want to give you my exact functions because I want you to do this from scratch. However, I'll give you the basic order of operations:
 
 Split the markdown into blocks (you already have a function for this)
 Loop over each block:
 Determine the type of block (you already have a function for this)
 Based on the type of block, create a new HTMLNode with the proper data
-Assign the proper child HTMLNode objects to the block node. I created a shared text_to_children(text) function that works for all block types. It takes a string of text and returns a list of HTMLNodes that represent the inline markdown using previously created functions (think TextNode -> HTMLNode).
-The "code" block is a bit of a special case: it should not do any inline markdown parsing of its children. I didn't use my text_to_children function for this block type, I manually made a TextNode and used text_node_to_html_node.
+Assign the proper child HTMLNode objects to the block node.
+ I created a shared text_to_children(text) function that works for all block types. 
+ It takes a string of text and returns a list of HTMLNodes that represent the inline markdown using previously created functions (think TextNode -> HTMLNode).
+The "code" block is a bit of a special case: it should not do any inline markdown parsing of its children. 
+I didn't use my text_to_children function for this block type, I manually made a TextNode and used text_node_to_html_node.
 Make all the block nodes children under a single parent HTML node (which should just be a div) and return it.
 
 """
@@ -18,4 +31,8 @@ Make all the block nodes children under a single parent HTML node (which should 
 
 
 def markdown_to_html_node(markdown):
+
+    # return list of blocks
+    list_blocks = markdown_to_blocks(markdown)
+    
     
