29cb87eb325f92a076b0d50e1cd6e82f4561d57d
Samuel-John-Wilson <Samuel.John.Wilson@outlook.com>
2025-06-15 20:10:56 +1000

    0.1.6: copy_directory written not tested

--- src/main.py
+++ src/main.py
@@ -7,21 +7,42 @@ from markdown_to_html import to_children, to_heading, to_paragraph, to_code, to_
 import os
 import shutil 
 
-def main():
-    node = TextNode("I ate ur dog", TextType.LINK, "http://iateurdog.blogspot.com")
-    print(node)
+dir_log =[]
 
-def copy_directory(directory):
-    if not os.path.exists(directory):
-        raise ValueError("Invalid directory")
+
+def copy_directory(src, destination):
+    global dir_log
+    if not os.path.exists(src):
+        raise ValueError("Invalid source directory")
+    # delete directory, then recreate it empty
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    os.makedirs(destination)
+    src_list = os.listdir(src)
+    for item in src_list:
+        file_path = os.path.join(src, item)
+        dest_path = os.path.join(destination, item)
+
+        if os.path.isfile(file_path):
+            dir_log.append(file_path)
+            shutil.copy(file_path, dest_path)
+        elif os.path.isdir(file_path):
+            dir_log.append(file_path)
+            copy_directory(file_path, dest_path)
+        else:
+            dir_log.append(f"{file_path} object was neither file nor directory")
+    print(dir_log)
+
+
+    
     
 
 
 
 
-
-
-
+def main():
+    node = TextNode("I ate ur dog", TextType.LINK, "http://iateurdog.blogspot.com")
+    print(node)
 
 if __name__=="__main__":
     main()
